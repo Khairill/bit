2be61e25e445231e20c09df78e3deb71c7bb202c
Khairill <55897154+Khairill@users.noreply.github.com>
2019-11-25 20:50:24 +0700

    Create config.php

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+
+$in = "_ga=GA1.2.861324686.1573843733; _gid=GA1.2.1858297366.1574662673; u_id=9024; PHPSESSID=e207fa99dc495f3251f5a2b3c0fc9848; _gat_gtag_UA_143322574_1=1";
+$task = "cardbit";
+$UserAgent = "User-Agent:Mozilla/5.0 (Linux; Android 9; vivo 1904) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Mobile Safari/537.36";
